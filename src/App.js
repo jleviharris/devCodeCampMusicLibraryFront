5ead46a0c6001b9190f1677eb14b2c3d20f9f9c6
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-04-08 21:31:03 -0400

    Finial Commit before submission

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import "./index.css";
-import SongTable from './Components/SongTable/songTable';
 import SearchBar from './Components/SearchBar/searchBar';
 import NavBar from './Components/NavBar/navBar';
-// import { render } from '@testing-library/react';
 import axios from 'axios';
 
-  
-
 function App() {
-
+  
   const [Songs, setSongs] = useState([]);
-
+  
   const getData = async () => {
     try{
-      let response = await axios.get('http://www.devcodecampmusiclibrary.com/api/music/');
+      let response = await axios.get('http://localHost:5005/api/songs');
       function placeSongs(){
         let tempSongs = [...response.data];
         setSongs(tempSongs);
       }
     placeSongs();
-    
     }
     catch (ex) {
       console.log('Error in API call');
